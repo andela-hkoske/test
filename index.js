e32ab5bb723850040d74536d038d4e2f97ebb3a4
hannahkoske <hannah.koske@andela.com>
2015-09-24 16:33:59 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function Index(){
+  this.createIndex = function(path){console.log("Creating Index..."+path);};
+  this.getIndex = function(){console.log("Returning Index...");};
+  this.searchIndex = function(){console.log("Searching Index...");};
+}
